8a36f5ff9e1411f6cfcb7d8d8ba4b4dcc9b32481
JavaGath <i.izrailtenko@web.de>
2023-04-29 21:02:03 +0200

    init project

---
+++ src/main/java/de/javagath/blogdbservice/BlogdbServiceApplication.java
@@ -0,0 +1,13 @@
+package de.javagath.blogdbservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogdbServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogdbServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/javagath/blogdbservice/BlogdbServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.javagath.blogdbservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogdbServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
